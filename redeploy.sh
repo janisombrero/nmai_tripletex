1461fabcf97e9b7193188a0aae6b9b047fa7c543
janisombrero <ojanio@gmail.com>
2026-03-22 07:15:48 +0000

    fix: read GOOGLE_API_KEY from .env in redeploy.sh

--- redeploy.sh
+++ redeploy.sh
@@ -11,7 +11,7 @@ gcloud run deploy nmiai \
   --min-instances 1 \
   --memory 1Gi \
   --timeout 300 \
-  --set-env-vars "GOOGLE_API_KEY=${GOOGLE_API_KEY}"
+  --set-env-vars "GOOGLE_API_KEY=$(grep GOOGLE_API_KEY .env | cut -d= -f2)"
 
 echo "Done. Testing health endpoint..."
 URL=$(gcloud run services describe nmiai --region europe-north1 --format="value(status.url)")
